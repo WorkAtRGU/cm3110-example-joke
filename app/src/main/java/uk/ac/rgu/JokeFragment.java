153b9d4ab24fa050f4b5ffc2ec4fa1a166bd128c
dcorsar <20Pfgh!19>
2021-11-24 14:53:22 +0000

    Initial set of action listener code.

--- app/src/main/java/uk/ac/rgu/JokeFragment.java
+++ app/src/main/java/uk/ac/rgu/JokeFragment.java
@@ -2,27 +2,26 @@ package uk.ac.rgu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link JokeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class JokeFragment extends Fragment {
+public class JokeFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mSetup;
+    private String mPunchline;
 
     public JokeFragment() {
         // Required empty public constructor
@@ -32,16 +31,13 @@ public class JokeFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment JokeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static JokeFragment newInstance(String param1, String param2) {
+    public static JokeFragment newInstance() {
         JokeFragment fragment = new JokeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +46,7 @@ public class JokeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // nothing to setup
         }
     }
 
@@ -61,4 +56,37 @@ public class JokeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_joke, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // add on click listener to the reveal button
+        Button revealButton = view.findViewById(R.id.btn_reveal);
+        revealButton.setOnClickListener(this);
+
+        // download a joke and update the ui
+        downloadJokeUpdateUI();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_reveal){
+            // launch the action to navigate to the punchline fragment
+        }
+    }
+
+    /**
+     * Downloads a joke, updates this object's member variables, and the UI
+     */
+    private void downloadJokeUpdateUI() {
+    }
+    
+    private int generateRandomNumber(){
+        // adated from https://www.educative.io/edpresso/how-to-generate-random-numbers-in-java
+        int max = 116;
+        Random random = new Random();
+        int value = random.nextInt(max);
+        return value;
+    }
+    
 }
